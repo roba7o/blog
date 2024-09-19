5a4452cf8b2ed0d66e5779cb35ce5c12e919ac13
roba7o <rhm14148@gmail.com>
2024-09-19 08:50:47 +0100

    view for form submission created

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,6 +3,36 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 
 from .models import Post
+from .forms import EmailPostForm
+
+def post_share(request, post_id):
+    # Retrieve post by id
+    post = get_object_or_404(
+        Post,
+        id=post_id,
+        status=Post.status.PUBLISHED
+    )
+
+    if request.method == "POST":
+        # Form was submitted
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            # form fields passed validation
+            cd = form.cleaned_data
+            # ...send email
+
+    else:
+        form = EmailPostForm()
+    
+    return render(
+        request, 
+        'blog/post/share.html',
+        {
+            'post': post,
+            'form': form
+        }
+    )
+    
 
 class PostListView(ListView):
     """
