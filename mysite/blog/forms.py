d45384a46737a4c0e97834a39c16cdde43d6fca3
roba7o <rhm14148@gmail.com>
2024-09-20 15:14:26 +0100

    unstaged form last commit ^^

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .models import Comment
 
 class EmailPostForm(forms.Form):
     name = forms.CharField(max_length=25)
@@ -8,4 +9,10 @@ class EmailPostForm(forms.Form):
         required=False,
         widget=forms.Textarea
     )
-    
+
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ['name', 'email', 'body']
+        
