84609c9ff7d03ca74c4fe174a2263fd5938b161b
roba7o <rhm14148@gmail.com>
2024-09-25 08:23:11 +0100

    RSS feed implemented (ugly XML)

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from . import views
+from . import views 
+from .feeds import LatestPostsFeed
 
 app_name = 'blog'
 
@@ -19,5 +20,6 @@ urlpatterns = [
     path(
         '<int:post_id>/comment/', views.post_comment, name="post_comment"
     ),
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
 
