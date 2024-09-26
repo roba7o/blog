51014cfbd3113161a240f4c36630924c8595a45e
roba7o <rhm14148@gmail.com>
2024-09-26 10:07:59 +0100

    sitemap includes tags

--- mysite/blog/sitemaps.py
+++ mysite/blog/sitemaps.py
@@ -1,5 +1,7 @@
 from django.contrib.sitemaps import Sitemap
+from django.urls import reverse
 from .models import Post
+from taggit.models import Tag
 
 class PostSitemap(Sitemap):
     changefreq = 'weekly'
@@ -10,4 +12,17 @@ class PostSitemap(Sitemap):
     
     def lastmod(self, obj):
         return obj.updated
+    
+class TagSitemap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.6
+
+    def items(self):
+        # Return all tags used in the posts
+        return Tag.objects.all()
+    
+    def location(self, obj):
+        # Generate URL for each tag
+        return reverse('blog:post_list_by_tag', args=[obj.slug])
+    
     
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,10 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, include
-from blog.sitemaps import PostSitemap
+from blog.sitemaps import PostSitemap, TagSitemap
 
 sitemaps = {
-    'posts': PostSitemap
+    'posts': PostSitemap,
+    'tags': TagSitemap,
 }
 
 urlpatterns = [
