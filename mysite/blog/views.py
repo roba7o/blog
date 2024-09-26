7b181fcb6ca2d8b79930bdac6674c7ecdc70c4d6
roba7o <rhm14148@gmail.com>
2024-09-26 08:19:27 +0100

    search view enhanced with SearchVecotr and SearchRank

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -3,7 +3,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 
 from .models import Post
 from taggit.models import Tag
@@ -173,11 +173,15 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
             results = (
                 Post.published.annotate(
-                    search=SearchVector('title', 'body'),
+                    search=search_vector,
+                    rank=SearchRank(search_vector, search_query)
                 )
-                .filter(search=query)
+                .filter(search=search_query)
+                .order_by('-rank')
             )
     return render(
         request,
